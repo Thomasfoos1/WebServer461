156cf45f3ff97c3cfb5ceb939e4796c791055031
Huy Tran <htt5059@psu.edu>
2021-10-26 17:24:09 -0400

    fix lib and while loop logic

---
+++ source/src/WebServer.java
@@ -0,0 +1,26 @@
+import java.net.* ;
+import java.util.* ;
+import webserver.HttpRequest;
+
+public final class WebServer {
+    public static void main(String argv[]) throws Exception {
+        int port = 6666;
+        
+        // Establish the listen socket with ServerSocket.
+        ServerSocket ss = new ServerSocket(port);
+        
+        // Provide a message for users.
+        System.out.println("The server is listening.");
+        // Process HTTP service requests in an infinite loop.
+        while (true) {
+            // Listen for a TCP connection request.
+            Socket s = ss.accept();
+            // Construct an object to process the HTTP request message.
+            HttpRequest request = new HttpRequest(s);
+            // Create a new thread to process the request.
+            Thread thread = new Thread(request);
+            // Start the thread.
+            thread.start();
+        }
+    }
+}
