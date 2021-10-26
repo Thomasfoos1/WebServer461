95668ab307351202c3ad92853768353e4e10ffc4
Thomasfoos1 <82347141+Thomasfoos1@users.noreply.github.com>
2021-10-26 16:31:27 -0400

    Add files via upload

---
+++ HttpRequest.java
@@ -0,0 +1,16 @@
+
+package webserver;
+
+import java.net.Socket;
+
+final public class HttpRequest implements Runnable {
+    
+    public HttpRequest (Socket socket) {
+        
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+}
