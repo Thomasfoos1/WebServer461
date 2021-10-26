a5a35e347a3df70abf6c7a51bbb5fed6dc3e6b05
Huy Tran <htt5059@psu.edu>
2021-10-26 17:10:03 -0400

    HttpRequest part 1

--- HttpRequest.java
+++ HttpRequest.java
@@ -20,6 +20,30 @@ final public class HttpRequest implements Runnable {
         }
     }
     private void processRequest() throws Exception {
-        
+        //Get a reference to the socket's input and output streams.
+        InputStream is= socket.getInputStream();
+        DataOutputStream os= new DataOutputStream(socket.getOutputStream);
+
+        //Set up input stream filters.
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = BufferedReader(isr);
+
+        //Get the request line of the HTTP request message.
+        String requestLine = br.readLine();
+
+        //Display the request line.
+        System.out.println();
+        System.out.println(requestLine);
+
+        //Get and display the header lines
+        String headerLine  = null;
+        while((headerLine=br.readLine().length()!=0)){
+            System.out.println(headerLine);
+        }
+
+        //Close streams and socket
+        socket.close();
+        is.close();
+        os.close();
     }
 }
